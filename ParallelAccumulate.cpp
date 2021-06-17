24997b46fb65c903d3027b587aabdb207a3af835
PatSos95 <patsos95@gmail.com>
2021-06-17 21:45:25 +0200

    Trying to wrap Accumulate with class. Not working

--- ParallelAccumulate.cpp
+++ ParallelAccumulate.cpp
@@ -6,49 +6,57 @@
 #include <cstdint>
 #include <functional>
 
-std::mutex accumulateMutex;
-void accumulate(unsigned long long& total_result, unsigned int lastNumber, unsigned int firstNumber = 1)
+class Accumulator
 {
-	unsigned long long result = 0;
-	while (lastNumber >= firstNumber)
+public:
+	void accumulate(unsigned long long& total_result, unsigned int lastNumber, unsigned int firstNumber = 1)
 	{
-		result += lastNumber;
-		lastNumber -= 1;
+		unsigned long long result = 0;
+		while (lastNumber >= firstNumber)
+		{
+			result += lastNumber;
+			lastNumber -= 1;
+		}
+
+		std::lock_guard<std::mutex> lock(accumulateMutex);
+		total_result += result;
 	}
 
-	std::lock_guard<std::mutex> lock(accumulateMutex);
-	total_result += result;
-}
-
-unsigned long long parallelAccumulate(unsigned int lastNumber)
-{
-	// Prepare variables
-	const unsigned int numOfThreads = std::thread::hardware_concurrency() - 1; //(1) - one thread for main thread
-	std::vector<std::future<void>> futuresVector;
-	const unsigned int numbersPerGroup = lastNumber / numOfThreads;
-	unsigned int firstNumberOfGroup;
-	std::future<void> future;
-	unsigned long long result = 0;
-
-	// Split set on threads
-	for (int i = 0; i < numOfThreads - 1; i++)
+	unsigned long long parallelAccumulate(unsigned int lastNumber)
 	{
-		firstNumberOfGroup = lastNumber - numbersPerGroup + 1;
-		future = std::async(std::launch::async, accumulate, std::ref(result), lastNumber, firstNumberOfGroup);
+		// Prepare variables
+		const unsigned int numOfThreads = std::thread::hardware_concurrency() - 1; //(1) - one thread for main thread
+		std::vector<std::future<void>> futuresVector;
+		const unsigned int numbersPerGroup = lastNumber / numOfThreads;
+		unsigned int firstNumberOfGroup;
+		std::future<void> future;
+		unsigned long long result = 0;
+
+		// Split set on threads
+		for (int i = 0; i < numOfThreads - 1; i++)
+		{
+			firstNumberOfGroup = lastNumber - numbersPerGroup + 1;
+			future = std::async(std::launch::async, Accumulator::accumulate, std::ref(result), lastNumber, firstNumberOfGroup);
+			futuresVector.push_back(std::move(future));
+			lastNumber = firstNumberOfGroup - 1;
+		}
+		future = std::async(std::launch::async, Accumulator::accumulate, std::ref(result), lastNumber, 1);
 		futuresVector.push_back(std::move(future));
-		lastNumber = firstNumberOfGroup - 1;
-	}
-	future = std::async(std::launch::async, accumulate, std::ref(result), lastNumber, 1);
-	futuresVector.push_back(std::move(future));
 
-	// Accumulate result in new thread
-	for (auto& f : futuresVector)
-	{
-		f.wait();
+		// Accumulate result in new thread
+		for (auto& f : futuresVector)
+		{
+			f.wait();
+		}
+
+		return result;
 	}
 
-	return result;
-}
+private:
+	std::mutex accumulateMutex;
+
+};
+
 
 int main()
 {
