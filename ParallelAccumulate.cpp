b33dc122927cf0243ad6abf87f02e34246f87cd8
PatSos95 <patsos95@gmail.com>
2021-07-01 19:19:43 +0200

    Working version

--- ParallelAccumulate.cpp
+++ ParallelAccumulate.cpp
@@ -46,7 +46,7 @@ private:
 		return queue.empty();
 	}
 
-	const std::size_t maxSize = 1;
+	const std::size_t maxSize = 3;
 	std::mutex queueMutex;
 	std::list<int> queue;
 	std::condition_variable queueNotFull;
@@ -58,10 +58,10 @@ class Producer
 public:
 	Producer(Queue& queue) : queue(queue) {}
 
-	auto run(unsigned int numberOfGeneratedNumbers) 
+	auto generateNumbers(unsigned int lastNumber, unsigned int firstNumber = 0)
 	{
 		return std::async([=, this] {
-			for (int i = 0; i < numberOfGeneratedNumbers + 1; i++) 
+			for (int i = firstNumber; i < lastNumber + 1; i++)
 			{ 
 				this->queue.push(i); 
 			}});
@@ -71,113 +71,52 @@ private:
 	Queue& queue;
 };
 
-class Consumer {
-public:
-	Consumer(Queue& queue) : queue(queue), result(0) {}
-
-	auto run(unsigned int numberOfNumbersToPop)
-	{
-		return std::async([=, this] {
-			for (int i = 0; i < numberOfNumbersToPop + 1; i++)
-			{
-				int number = this->queue.pop();
-				result += number;
-			}
-			return result; 
-		});
-	}
-
-private:
-	Queue& queue;
-	long long result;
-};
-
 
 class Accumulator
 {
 public:
-	void accumulate(unsigned long long& total_result, unsigned int lastNumber, unsigned int firstNumber = 1)
+	Accumulator(Queue& queue) : queue(queue) {}
+
+	auto accumulate(unsigned int lastNumber, unsigned int firstNumber = 0)
 	{
-		unsigned long long result = 0;
-		while (lastNumber >= firstNumber)
-		{
-			result += lastNumber;
-			lastNumber -= 1;
-		}
-
-		std::lock_guard<std::mutex> lock(accumulateMutex);
-		total_result += result;
-	}
-
-	unsigned long long parallelAccumulate(unsigned int lastNumber)
-	{
-		// Prepare variables
-		const unsigned int numOfThreads = std::thread::hardware_concurrency() - 1; // (1) - one thread for main thread
-		std::vector<std::future<void>> futuresVector;
-		const unsigned int numbersPerGroup = lastNumber / numOfThreads;
-		unsigned int firstNumberOfGroup;
-		std::future<void> future;
-		unsigned long long result = 0;
-
-		// Split set on threads
-		for (int i = 0; i < numOfThreads - 1; i++)
-		{
-			firstNumberOfGroup = lastNumber - numbersPerGroup + 1;
-			future = std::async(std::launch::async, [=, &result] {this->accumulate(std::ref(result), lastNumber, firstNumberOfGroup); });
-			futuresVector.push_back(std::move(future));
-			lastNumber = firstNumberOfGroup - 1;
-		}
-		future = std::async(std::launch::async, [=, &result] {this->accumulate(std::ref(result), lastNumber, 1); });
-		futuresVector.push_back(std::move(future));
-
-		// Accumulate result in new thread
-		for (auto& f : futuresVector)
-		{
-			f.wait();
-		}
-
-		return result;
+		return std::async([=, this] {
+			unsigned long long result = 0;
+			for (int i = firstNumber; i <= lastNumber; i++)
+			{
+				int number = this->queue.pop();
+				result += number;
+			}
+			return result;
+		});
 	}
 
 private:
 	std::mutex accumulateMutex;
+	Queue& queue;
 };
 
 int main()
 {
-	unsigned int number = 100;
-
-	Queue q;
-	Producer p(q);
-	Consumer c(q);
-
-	auto future_producer = p.run(number);
-	auto future_consumer = c.run(number);
-
-	future_producer.wait();
-	std::cout << future_consumer.get() << "\n\n";
-
-
-	Accumulator acc;
-
-	// Measure time of normal accumulate
-	auto start = std::chrono::system_clock::now();
+	unsigned int number = 100000;
 	unsigned long long result = 0;
-	acc.accumulate(result, number);
-	std::cout << result << "\n";
-	auto end = std::chrono::system_clock::now();
-	auto elapsed = (end - start).count();
-	std::cout << "Elapsed time of normal accumulate: " << elapsed << "\n\n";
 
-	// Mesure time of parallel accumulate
-	start = std::chrono::system_clock::now();
-	std::cout << acc.parallelAccumulate(number) << "\n";
-	end = std::chrono::system_clock::now();
-	auto elapsed2 = (end - start).count();
-	std::cout << "Elapsed time of parallel accumulate: " << elapsed2 << "\n\n";
+	Queue queue;
+	Producer producer1(queue);
+	Producer producer2(queue);
+	Producer producer3(queue);
+	Accumulator acc1(queue);
+	Accumulator acc2(queue);
 
-	float ratio = static_cast<float>(elapsed) / elapsed2;
-	std::cout << "Parallel version is " << ratio << "x faster" << "\n\n\n";
+	auto futureProducer1 = producer1.generateNumbers(number / 3);
+	auto futureProducer2 = producer2.generateNumbers((number / 3) * 2, (number / 3) + 1);
+	auto futureProducer3 = producer3.generateNumbers(number, (number / 3) * 2 + 1);
+	auto futureAcc1 = acc1.accumulate(number / 2);
+	auto futureAcc2 = acc2.accumulate(number, (number / 2) + 1);
+
+	futureProducer1.wait();
+	futureProducer2.wait();
+	futureProducer3.wait();
+	std::cout << futureAcc1.get() + futureAcc2.get() << "\n\n";
 
 	return 0;
 }
